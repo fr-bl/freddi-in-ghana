4d11b97c9c5adc7bd580f26dcc91a41042df8937
Frederick Bläsing <natrium@frederick.blaes.ing>
2025-11-11 20:12:16 +0000

    fix: fix image width and change mobile layout threshold

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://www.freddi-in-ghana.de",
     integrations: [icon(), mdx()],
     image: {
         service: import.meta.env.DEV ? passthroughImageService() : undefined,
