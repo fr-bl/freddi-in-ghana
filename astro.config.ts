32e86660176b844f1f5f6948220d32875358a218
Frederick Bläsing <natrium@frederick.blaes.ing>
2025-11-11 22:32:11 +0000

    feat: inline critical css

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import inline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.freddi-in-ghana.de",
-    integrations: [icon(), mdx()],
+    integrations: [icon(), mdx(), inline()],
     image: {
-        service: import.meta.env.DEV ? passthroughImageService() : undefined,
+        service: true ? passthroughImageService() : undefined,
     },
     vite: {
         server: {
