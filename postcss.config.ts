a2d8d2f653127035bfd47e8d82127e288442a4ef
Frederick Bläsing <natrium@frederick.blaes.ing>
2025-11-12 00:24:22 +0000

    fix: fix mobile layout media queries

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,11 +1,12 @@
 import atImport from "postcss-import";
-import cssVariables from "postcss-css-variables";
-import autoprefixer from "autoprefixer";
 import nesting from "postcss-nesting";
+import cssVariables from "postcss-css-variables";
+import customMedia from "postcss-custom-media";
+import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 
 export default {
   plugins: [atImport, nesting, cssVariables({
     preserve: (declaration) => declaration.value.includes("--font"),
-  }), autoprefixer, cssnano],
+  }), customMedia, autoprefixer, cssnano],
 };
